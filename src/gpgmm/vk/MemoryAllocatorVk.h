800f5de2d354815621298c156cf9e13102d97f2f
Bryan Bernhart <bryan.bernhart@intel.com>
2022-04-15 13:48:51 -0700

    WIP

---
+++ src/gpgmm/vk/MemoryAllocatorVk.h
@@ -0,0 +1,44 @@
+// Copyright 2021 The GPGMM Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef GPGMM_VK_MEMORYALLOCATORVK_H_
+#define GPGMM_VK_MEMORYALLOCATORVK_H_
+
+#include "gpgmm/MemoryAllocator.h"
+#include "gpgmm/common/Flags.h"
+#include "include/gpgmm_export.h"
+
+namespace gpgmm {
+    class MemoryAllocator;
+}  // namespace gpgmm
+
+namespace gpgmm { namespace vk {
+
+    class GPGMM_EXPORT MemoryAllocatorVk final : public MemoryAllocator {
+      public:
+        std::unique_ptr<MemoryAllocation> TryAllocateMemory(uint64_t allocationSize,
+                                                            uint64_t alignment,
+                                                            bool neverAllocate,
+                                                            bool cacheSize,
+                                                            bool prefetchMemory) override {
+            return {};
+        }
+
+        void DeallocateMemory(std::unique_ptr<MemoryAllocation> allocation) override {
+        }
+    };
+
+}}  // namespace gpgmm::vk
+
+#endif  // GPGMM_VK_MEMORYALLOCATORVK_H_
